7a32317294588c9abd954a050270988992586cb5
podhmo <ababjam61+github@gmail.com>
2019-01-23 20:56:07 +0900

    add tests

---
+++ try_request_test.go
@@ -0,0 +1,26 @@
+package webtest_test
+
+import (
+	"net/http"
+	"testing"
+
+	webtest "github.com/podhmo/go-webtest"
+)
+
+func TestTryRequest(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		handler := http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(`{"message": "ok"}`))
+			},
+		)
+		webtest.TryRequest(
+			t,
+			handler,
+			"GET",
+			"/",
+			http.StatusOK,
+		)
+	})
+}
