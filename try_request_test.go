d949d78ef8e8c0bb28806028d1533c0edb766561
podhmo <ababjam61+github@gmail.com>
2019-01-25 03:45:37 +0900

    test: more tests

--- try_request_test.go
+++ try_request_test.go
@@ -8,11 +8,57 @@ import (
 )
 
 func TestTryRequest(t *testing.T) {
-	t.Run("ok", func(t *testing.T) {
+	t.Run("response", func(t *testing.T) {
 		handler := http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
 				w.WriteHeader(http.StatusOK)
-				w.Write([]byte(`{"message": "ok"}`))
+				w.Write([]byte(`{"message": "ok", "id": "1"}`))
+			},
+		)
+
+		webtest.TryJSONRequest(
+			t,
+			handler,
+			"GET",
+			"/",
+			http.StatusOK,
+			webtest.WithAssertJSONResponse(`{"message": "ok", "id": "1"}`),
+		)
+	})
+
+	t.Run("modifyRequest", func(t *testing.T) {
+		handler := http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				username, password, ok := r.BasicAuth()
+				if !ok {
+					t.Fatalf("expecting, basic auth is set. but not set. header %v", r.Header)
+				}
+
+				if expected, actual := "*username*", username; expected != actual {
+					t.Errorf("invalid username, expected=%q, actual=%q", expected, actual)
+				}
+				if expected, actual := "*password*", password; expected != actual {
+					t.Errorf("invalid password, expected=%q, actual=%q", expected, actual)
+				}
+			},
+		)
+
+		webtest.TryJSONRequest(
+			t,
+			handler,
+			"GET",
+			"/",
+			http.StatusOK,
+			webtest.WithModifyRequest(func(req *http.Request) {
+				req.SetBasicAuth("*username*", "*password*")
+			}),
+		)
+	})
+	t.Run("assertFunc", func(t *testing.T) {
+		handler := http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("location", "/newplace")
+				w.WriteHeader(http.StatusSeeOther)
 			},
 		)
 		webtest.TryJSONRequest(
@@ -20,8 +66,13 @@ func TestTryRequest(t *testing.T) {
 			handler,
 			"GET",
 			"/",
-			http.StatusOK,
-			webtest.WithAssertJSONResponse(`{"message": "ok"}`),
+			http.StatusSeeOther,
+			webtest.WithAssertFunc(func(t testing.TB, output *webtest.TryRequestOutput) {
+				location := output.Response.Header.Get("location")
+				if expected, actual := "/newplace", location; expected != actual {
+					t.Errorf("invalid redirect-location, expected=%q, actual=%q", expected, actual)
+				}
+			}),
 		)
 	})
 }
