98be89a282778bd439e2da2dcfbbda32edbfdfa0
podhmo <ababjam61+github@gmail.com>
2019-07-27 17:20:10 +0900

    tests: add integration tests

---
+++ snapshot/integration_test.go
@@ -0,0 +1,16 @@
+package snapshot_test
+
+import (
+	"testing"
+
+	"github.com/podhmo/go-webtest/jsonequal"
+	"github.com/podhmo/go-webtest/snapshot"
+)
+
+func TestIt(t *testing.T) {
+	got := snapshot.GetData() // see: helper_test.go
+	want := snapshot.Take(t, got)
+	if err := jsonequal.Equal(jsonequal.From(got), jsonequal.From(want)); err != nil {
+		t.Errorf("%+v", err)
+	}
+}
