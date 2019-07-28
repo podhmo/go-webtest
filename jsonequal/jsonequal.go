ded994d043c9e547261d46a6811bfabb9e6ea13b
podhmo <ababjam61+github@gmail.com>
2019-07-28 22:15:59 +0900

    functions, that jsonequal for performance (#13)
    * feat: add jsonequal.FromRaw()
    
    * feat: with bytes

--- jsonequal/jsonequal.go
+++ jsonequal/jsonequal.go
@@ -31,6 +31,24 @@ func From(iface interface{}) func() (interface{}, []byte, error) {
 	}
 }
 
+// FromRaw :
+func FromRaw(iface interface{}) func() (interface{}, []byte, error) {
+	return func() (interface{}, []byte, error) {
+		b, err := json.Marshal(iface)
+		if err != nil {
+			return nil, nil, err
+		}
+		return iface, b, nil
+	}
+}
+
+// FromRawWithBytes :
+func FromRawWithBytes(iface interface{}, b []byte) func() (interface{}, []byte, error) {
+	return func() (interface{}, []byte, error) {
+		return iface, b, nil
+	}
+}
+
 // FromReader :
 func FromReader(reader io.Reader) func() (interface{}, []byte, error) {
 	return func() (interface{}, []byte, error) {
