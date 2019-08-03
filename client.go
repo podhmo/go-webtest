cb9766e1b76a893a27645a429f58d9dab1905f07
podhmo <ababjam61+github@gmail.com>
2019-08-04 03:22:11 +0900

    feat: update Client interface

--- client.go
+++ client.go
@@ -22,78 +22,85 @@ type Internal interface {
 // Client :
 type Client struct {
 	Internal Internal
+	Config   *Config
+}
+
+// Requset :
+func (c *Client) Request(
+	path string,
+	options ...func(*Config),
+) (Response, error, func()) {
+	config := c.Config.Copy()
+	for _, opt := range options {
+		opt(config)
+	}
+	method := config.Method
+	body := config.body
+	return c.Internal.Request(method, path, body)
 }
 
 // Do :
-func (c *Client) Do(req *http.Request) (Response, error, func()) {
+func (c *Client) Do(
+	req *http.Request,
+) (Response, error, func()) {
 	return c.Internal.Do(req)
-}
-
-// Get :
-func (c *Client) Get(path string) (Response, error, func()) {
-	return c.Internal.Request("GET", path, nil)
-}
-
-// Head :
-func (c *Client) Head(path string) (Response, error, func()) {
-	return c.Internal.Request("HEAD", path, nil)
-}
-
-// Post :
-func (c *Client) Post(
-	path, contentType string,
-	body io.Reader,
-) (Response, error, func()) {
-	return c.Internal.Request("POST", path, body, func(req *http.Request) {
-		req.Header.Set("Content-Type", contentType)
-	})
-}
-
-// PostForm :
-func (c *Client) PostForm(
-	path string,
-	data url.Values,
-) (Response, error, func()) {
-	return c.Post(
-		path,
-		"application/x-www-form-urlencoded",
-		strings.NewReader(data.Encode()),
-	)
-}
-
-// PostJSON :
-func (c *Client) PostJSON(
-	path string,
-	body io.Reader,
-) (Response, error, func()) {
-	return c.Post(path, "application/json", body)
 }
 
 // NewClientFromTestServer :
 func NewClientFromTestServer(ts *httptest.Server, options ...func(*Config)) *Client {
-	c := &Config{}
+	config := NewConfig()
 	for _, opt := range options {
-		opt(c)
+		opt(config)
 	}
 	return &Client{
 		Internal: &client.HTTPTestServerClient{
 			Server:   ts,
-			BasePath: c.BasePath,
+			BasePath: config.BasePath,
 		},
+		Config: config,
 	}
 }
 
 // NewClientFromHandler :
 func NewClientFromHandler(handlerFunc http.HandlerFunc, options ...func(*Config)) *Client {
-	c := &Config{}
+	config := NewConfig()
 	for _, opt := range options {
-		opt(c)
+		opt(config)
 	}
 	return &Client{
 		Internal: &client.HTTPTestResponseRecorderClient{
 			HandlerFunc: handlerFunc,
-			BasePath:    c.BasePath,
+			BasePath:    config.BasePath,
 		},
+		Config: config,
+	}
+}
+
+// Config :
+type Config struct {
+	BasePath string
+
+	Method          string
+	RequstModifiers []func(*http.Request)
+
+	body io.Reader // only once
+}
+
+// NewConfig :
+func NewConfig() *Config {
+	return &Config{
+		Method: "GET",
+	}
+}
+
+// Config :
+func (c *Config) Copy() *Config {
+	return &Config{
+		BasePath: c.BasePath,
+		RequstModifiers: append(
+			make([]func(*http.Request), 0, len(c.RequstModifiers)),
+			c.RequstModifiers...,
+		),
 	}
 }
 
@@ -104,7 +111,35 @@ func WithBasePath(basePath string) func(*Config) {
 	}
 }
 
-// Config :
-type Config struct {
-	BasePath string
+// WithForm :
+func WithForm(data url.Values) func(*Config) {
+	return func(c *Config) {
+		if c.body != nil {
+			panic("body is already set, enable to set body only once") // xxx
+		}
+		c.body = strings.NewReader(data.Encode())
+		c.RequstModifiers = append(c.RequstModifiers, func(req *http.Request) {
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		})
+	}
+}
+
+// WithJSON :
+func WithJSON(body io.Reader) func(*Config) {
+	return func(c *Config) {
+		if c.body != nil {
+			panic("body is already set, enable to set body only once") // xxx
+		}
+		c.body = body
+		c.RequstModifiers = append(c.RequstModifiers, func(req *http.Request) {
+			req.Header.Set("Content-Type", "application/json")
+		})
+	}
+}
+
+// AddModifyRequest :
+func AddModifyRequest(modify func(*http.Request)) func(*Config) {
+	return func(c *Config) {
+		c.RequstModifiers = append(c.RequstModifiers, modify)
+	}
 }
