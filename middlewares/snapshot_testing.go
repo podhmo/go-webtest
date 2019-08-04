e25117dce95b7b96920b59d4bb169a59a064c696
podhmo <ababjam61+github@gmail.com>
2019-08-04 21:01:02 +0900

    feat: add TakeSnapshot() (#33)
    * feat: add TakeSnapshot()
    
    * style: rename function

--- middlewares/snapshot_testing.go
+++ middlewares/snapshot_testing.go
@@ -17,23 +17,45 @@ func SnapshotTesting(want *interface{}, options ...func(sc *snapshot.Config)) fu
 				res Response,
 				req *http.Request,
 			) error {
-				// TODO: following .har structure?
-
-				storedata := map[string]interface{}{
-					"request": map[string]interface{}{
-						"method": req.Method,
-						"path":   req.URL.Path + req.URL.RawQuery,
-					},
-					"response": map[string]interface{}{
-						"statusCode": res.StatusCode(),
-						"data":       res.JSONData(),
-					},
-				}
+				storedata := createSnapshotData(res, req)
 
 				// assign (side-effect!!), want is response data
 				loaddata := snapshot.Take(t, storedata, options...)
 				*want = loaddata.(map[string]interface{})["response"].(map[string]interface{})["data"]
 				return nil
 			}))
+	}
+}
+
+// TakeSnapshot always takes a snapshot
+func TakeSnapshot(want *interface{}, options ...func(sc *snapshot.Config)) func(*webtest.Config) {
+	return func(c *webtest.Config) {
+		c.Middlewares = append(c.Middlewares, NewMiddleware(
+			func(
+				t testing.TB,
+				res Response,
+				req *http.Request,
+			) error {
+				storedata := createSnapshotData(res, req)
+				_ = snapshot.Take(t,
+					storedata,
+					append([]func(*snapshot.Config){snapshot.WithForceUpdate()}, options...)...,
+				)
+				return nil
+			}))
+	}
+}
+
+func createSnapshotData(res Response, req *http.Request) interface{} {
+	// TODO: following .har structure? or openapi spec structure?
+	return map[string]interface{}{
+		"request": map[string]interface{}{
+			"method": req.Method,
+			"path":   req.URL.Path + req.URL.RawQuery,
+		},
+		"response": map[string]interface{}{
+			"statusCode": res.StatusCode(),
+			"data":       res.JSONData(),
+		},
 	}
 }
