382f2905022b7f40c351ee4a36e8ab7673a0025e
podhmo <ababjam61+github@gmail.com>
2019-09-07 22:07:09 +0900

    feat: rewrite try

--- try.go
+++ try.go
@@ -3,40 +3,58 @@ package webtest
 import (
 	"testing"
 
+	"github.com/podhmo/go-webtest/jsonequal"
+	"github.com/podhmo/go-webtest/tripperware"
 	"github.com/podhmo/noerror"
 )
 
-// Assertion :
-type Assertion = func(t *testing.T, got Response)
-
 // Try :
-func Try(t *testing.T, assertions ...Assertion) *TryWithAssertion {
-	var args []Assertion
-	for _, arg := range assertions {
-		if arg == nil {
-			continue
+type Try struct {
+	_Required_Sentinel struct{}
+
+	Code int
+
+	Data           *interface{}
+	ModifyResponse func(res Response) (actual interface{})
+}
+
+// Do :
+func (expected Try) Do(
+	t *testing.T,
+	c *Client,
+	method string,
+	path string,
+	options ...Option,
+) {
+	t.Helper()
+	needSnapshot := expected.Data != nil
+
+	if expected.Code != 0 {
+		options = append(options, WithTripperware(tripperware.ExpectCode(t, expected.Code)))
+	}
+	if needSnapshot {
+		options = append(options, WithTripperware(tripperware.GetExpectedDataFromSnapshot(t, expected.Data)))
+	}
+
+	got, err := c.Do(method, path, options...)
+	noerror.Must(t, err)
+	defer func() { noerror.Must(t, got.Close()) }()
+
+	if needSnapshot {
+		noerror.Must(t, noerror.NotEqual(nil).Actual(expected.Data))
+
+		var actual interface{}
+		if expected.ModifyResponse != nil {
+			actual = expected.ModifyResponse(got)
+		} else {
+			actual = got.JSONData()
 		}
-		args = append(args, arg)
-	}
-	return &TryWithAssertion{
-		t:          t,
-		assertions: args,
-	}
-}
 
-// TryWithAssertion :
-type TryWithAssertion struct {
-	t          *testing.T
-	assertions []Assertion
-}
-
-// With :
-func (a *TryWithAssertion) With(got Response, err error) {
-	a.t.Helper()
-	noerror.Must(a.t, err)
-	defer func() { noerror.Should(a.t, got.Close()) }()
-
-	for _, assert := range a.assertions {
-		assert(a.t, got)
+		noerror.Should(t,
+			jsonequal.ShouldBeSame(
+				jsonequal.From(actual),
+				jsonequal.From(expected.Data),
+			),
+		)
 	}
 }
