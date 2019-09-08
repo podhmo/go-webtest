3a0e0fbf551bd50652a77bbd12f9227e7776398a
podhmo <ababjam61+github@gmail.com>
2019-09-08 09:45:17 +0900

    Refine expect code (#64)
    * fix: refine expect-code's message
    
    * docs: update readme

--- tripperware/expect_code.go
+++ tripperware/expect_code.go
@@ -1,10 +1,10 @@
 package tripperware
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net/http"
+	"net/http/httputil"
+	"strings"
 	"testing"
 )
 
@@ -20,12 +20,11 @@ func ExpectCode(t testing.TB, code int) Ware {
 				return res, nil
 			}
 
-			defer res.Body.Close()
-			var b bytes.Buffer
-			if _, err := io.Copy(&b, res.Body); err != nil {
+			b, err := httputil.DumpResponse(res, true)
+			if err != nil {
 				return nil, err
 			}
-			return nil, &statusError{want: code, response: res, text: b.String()}
+			return nil, &statusError{want: code, response: res, text: string(b)}
 		})
 	}
 }
@@ -38,12 +37,19 @@ type statusError struct {
 }
 
 func (err *statusError) Error() string {
+	var b strings.Builder
+	w := &b
+
+	fmt.Fprintln(w, "\x1b[32mResponse: ------------------------------\x1b[0m")
+	fmt.Fprint(w, err.text)
+	fmt.Fprintln(w, "\x1b[32m----------------------------------------\x1b[0m")
+
 	return fmt.Sprintf(
-		"status code, got is \"%[1]d %[2]s\", but want is \"%[3]d %[4]s\"\n response is %[5]s",
+		"status code, got is \"%[1]d %[2]s\", but want is \"%[3]d %[4]s\"\n%[5]s",
 		err.response.StatusCode,
 		http.StatusText(err.response.StatusCode),
 		err.want,
 		http.StatusText(err.want),
-		err.text,
+		w.String(),
 	)
 }
